b53143c99fe936795a2493dac6b5e9790f2683b0
patvega <patrickjvega02@gmail.com>
2023-03-16 16:11:06 -0400

    fixing userid issue

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const User = require("../models/profileSchema.js")
+const { Types } = require('mongoose');
 
-
+//retrieve user
 router.get('/:userId', async (request,response) => {
     try {
         if (request.params.userId.length != 24) {
-          response.status(400).json({ message: "Invalid ID" });
+          response.status(400).json({ message: "Invalid ID Parameter" });
         } else {
-          let user = await User.findById(request.params.userId);
-          Reflect.deleteProperty(user, 'saltedHashedPass')
-          console.log(user);
+          const user = await User.findById(request.params.userId);
           if (user === null) {
             response.status(404).json({ message: "User Not Found" });
           } else {
@@ -24,5 +23,57 @@ router.get('/:userId', async (request,response) => {
       }
 })
 
+//update user analytics
+// router.patch('/:userId', async (request,response) => {
+//   try {
+//       if (request.params.userId.length != 24) {
+//         response.status(400).json({ message: "Invalid ID Parameter" });
+//       } else {
+//         const user = await User.findById(request.params.userId);
+//         if (user === null) {
+//           response.status(404).json({ message: "User Not Found" });
+//         } else {
+//           response.status(200);
+//           response.send(user);
+//         }
+//       }
+//     } catch (e) {
+//       response.status(500).json({ message: "Internal Error" });
+//       console.log(e);
+//     }
+// })
+
+//delete user analytics
+// router.delete('/:userId', async (request,response) => {
+//   try {
+//       if (request.params.userId.length != 24) {
+//         response.status(400).json({ message: "Invalid ID Parameter" });
+//       } else {
+//         const user = await User.findById(request.params.userId);
+//         if (user === null) {
+//           response.status(404).json({ message: "User Not Found" });
+//         } else {
+//           response.status(200);
+//           response.send(user);
+//         }
+//       }
+//     } catch (e) {
+//       response.status(500).json({ message: "Internal Error" });
+//       console.log(e);
+//     }
+// })
+
+
+//router.get('/:userId/analytics', async (request, response) => {
+
+
+
+
+
+
+
+
+
+//})
 
 module.exports = router
