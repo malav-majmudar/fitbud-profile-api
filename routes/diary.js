546841f6e310e4a527b13443ba27321681334298
patvega <patrickjvega02@gmail.com>
2023-03-19 23:44:00 -0400

    Implementing diary api

--- routes/diary.js
+++ routes/diary.js
@@ -61,9 +61,7 @@ router.patch("/:diaryId", async (request, response) => {
     const diaryId = request.params.diaryId
     const diary = await Diary.findById(diaryId)
     try{
-        console.log("diary userId: ", diary.userId, " and type: ", typeof diary.userId)
-        console.log("request userId: ", request.body.userId, " and type: ", typeof new mongoose.Types.ObjectId(request.body.userId))
-        if(diary.userId !== new mongoose.Types.ObjectId(request.body.userId)) {
+        if(String(diary.userId) !== request.body.userId) {
             return response.status(400).send({ message: "Invalid user access!" });
         }
         else if(request.params.diaryId.length != 24) {
