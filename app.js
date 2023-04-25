82647c7723e0e5ef1994468410dfd6867fd58d02
patvega <patrickjvega02@gmail.com>
2023-04-25 13:50:16 -0400

    Modifying profile picture route!

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 
 mongoose.set("strictQuery", true);
